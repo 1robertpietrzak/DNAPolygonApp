02636efce9051751464403e5f54eac53790e3017
Robert <robzii17@gmail.com>
2019-09-17 20:32:08 +0200

    Initial Commit

---
+++ src/main/java/pl/eniac/DNAPolygonApp/DnaPolygonAppApplication.java
@@ -0,0 +1,13 @@
+package pl.eniac.DNAPolygonApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnaPolygonAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnaPolygonAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/eniac/DNAPolygonApp/DnaPolygonAppApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.eniac.DNAPolygonApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DnaPolygonAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
